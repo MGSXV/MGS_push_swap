fce129254719194db73cbe42066c318eee3b5520
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-04-04 22:32:29 +0000

    Stack struct

--- includes/headers/types.h
+++ includes/headers/types.h
@@ -6,9 +6,12 @@
 /*   By: sel-kham <sel-kham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/30 16:55:57 by sel-kham          #+#    #+#             */
-/*   Updated: 2022/03/30 18:12:23 by sel-kham         ###   ########.fr       */
+/*   Updated: 2022/04/04 03:10:14 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+# include "../libft/libft.h"
+# include "../ft_printf/ft_printf.h"
 
 struct s_node
 {
@@ -16,3 +19,9 @@ struct s_node
 	struct s_node	*previous_node;
 	int				data;
 }	typedef t_node;
+
+struct s_stack
+{
+	t_list	*head;
+	t_list	*tail;
+}	typedef t_stack;
