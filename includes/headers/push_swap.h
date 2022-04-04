af8dff52cd1883829bc0b0a9b0bbc4f602983cac
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-04-04 22:32:45 +0000

    Restructiong and ading fuctions

--- includes/headers/push_swap.h
+++ includes/headers/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: sel-kham <sel-kham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/29 22:36:54 by sel-kham          #+#    #+#             */
-/*   Updated: 2022/03/30 16:56:15 by sel-kham         ###   ########.fr       */
+/*   Updated: 2022/04/04 21:10:19 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,10 @@
 # define PUSH_SWAP_H
 
 #include "types.h"
-# include "../libft/libft.h"
-# include "../ft_printf/ft_printf.h"
 
 void	ft_error(const char *err_msg);
+t_node	*ft_new_node(int data);
+void	ft_add_node_at_first(t_node *new_node, t_node **head);
+void	ft_push_to_stack(t_node **head, char **v, int c);
 
 #endif
