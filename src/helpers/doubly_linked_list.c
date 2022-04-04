8f13cd241f654548ee34d6a3633c331b221832d7
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-04-04 22:33:29 +0000

    Creating half circular / doubly-linked list

--- src/helpers/doubly_linked_list.c
+++ src/helpers/doubly_linked_list.c
@@ -6,7 +6,7 @@
 /*   By: sel-kham <sel-kham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/30 18:06:34 by sel-kham          #+#    #+#             */
-/*   Updated: 2022/03/31 21:52:45 by sel-kham         ###   ########.fr       */
+/*   Updated: 2022/04/04 22:00:09 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,14 +25,21 @@ t_node	*ft_new_node(int data)
 	return (new_node);
 }
 
-void	ft_add_node_at_first(int data, t_node **head)
+void	ft_add_node_at_first(t_node *new_node, t_node **head)
 {
-	t_node	*new_node;
+	static t_node	*tail;
 
-	new_node = ft_new_node(data);
-	if (!new_node)
-		return ;
-	(*head)->next_node->previous_node = new_node;
-	new_node->next_node = (*head);
-	(*head) = new_node;
+	
+	if (!(*head))
+	{
+		*head = new_node;
+		tail = new_node;
+	}
+	else
+	{
+		(*head)->previous_node = new_node;
+		new_node->next_node = *head;
+		*head = new_node;
+		(*head)->previous_node = tail;
+	}
 }
