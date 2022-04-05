48fde0576e0fd7ba8633b85a7847dcd8ed9ba157
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-04-05 23:06:08 +0000

    ft_push_to_stack takes t_stack rather than t_node

--- src/app/manage_stacks.c
+++ src/app/manage_stacks.c
@@ -6,17 +6,17 @@
 /*   By: sel-kham <sel-kham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/04 22:21:54 by sel-kham          #+#    #+#             */
-/*   Updated: 2022/04/04 22:30:44 by sel-kham         ###   ########.fr       */
+/*   Updated: 2022/04/05 22:48:56 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/headers/push_swap.h"
 
-void	ft_push_to_stack(t_node **head, char **v, int c)
+void	ft_push_to_stack(t_stack **stack, char **v, int c)
 {
 	int		i;
 
 	i = c;
 	while (--i > 0)
-		ft_add_node_at_first(ft_new_node(ft_atoi(v[i])), head);
+		ft_add_node_at_first(ft_new_node(ft_atoi(v[i])), stack);
 }
