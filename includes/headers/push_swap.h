5562b75dac70de8135ba613c9a8396a2ace25a79
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-04-06 20:45:57 +0000

    Add ft_handle_args function

--- includes/headers/push_swap.h
+++ includes/headers/push_swap.h
@@ -6,16 +6,18 @@
 /*   By: sel-kham <sel-kham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/29 22:36:54 by sel-kham          #+#    #+#             */
-/*   Updated: 2022/04/05 23:15:44 by sel-kham         ###   ########.fr       */
+/*   Updated: 2022/04/06 20:29:13 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
+# include<limits.h>
 # include "types.h"
 
 void	ft_error(const char *err_msg);
+void	ft_handle_args(int ac, char *av);
 t_node	*ft_new_node(int data);
 void	ft_add_node_at_first(t_stack **stack, t_node *new_node);
 void	ft_push_to_stack(t_stack **stack, char **v, int c);
