d06b9865f6c931a3db10674711c30ad6a89aff02
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-04-06 20:46:13 +0000

    Create ft_handle_args function

--- src/helpers/ft_error.c
+++ src/helpers/ft_error.c
@@ -6,7 +6,7 @@
 /*   By: sel-kham <sel-kham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/30 16:51:38 by sel-kham          #+#    #+#             */
-/*   Updated: 2022/04/05 23:14:40 by sel-kham         ###   ########.fr       */
+/*   Updated: 2022/04/06 20:33:15 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,4 +16,21 @@ void	ft_error(const char *err_msg)
 {
 	ft_printf("%s", err_msg);
 	exit(EXIT_FAILURE);
+}
+
+void	ft_handle_args(int ac, char *av)
+{
+	if (ac == 1)
+		ft_error("Invalid number of arguments!\n\
+Try \"--help\" for more information.\n");
+	if (ac == 2)
+	{
+		if (!ft_strncmp(av, "--help", 6))
+		{
+			ft_error("\"push_swap\" requires minimum of one argument \
+passed as fllows:\n$> ./push_swap arg1 arg2 arg3 arg4 ...\n");
+		}
+		else
+			ft_error("Error!\n");
+	}
 }
