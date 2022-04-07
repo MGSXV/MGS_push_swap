930e78a3b9f63b282e073b506179161d476018c0
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-04-07 01:14:35 +0000

    Remove ft_handle_args function

--- src/helpers/ft_error.c
+++ src/helpers/ft_error.c
@@ -6,7 +6,7 @@
 /*   By: sel-kham <sel-kham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/30 16:51:38 by sel-kham          #+#    #+#             */
-/*   Updated: 2022/04/06 22:57:38 by sel-kham         ###   ########.fr       */
+/*   Updated: 2022/04/07 01:14:05 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,22 +16,4 @@ void	ft_error(const char *err_msg)
 {
 	ft_printf("%s", err_msg);
 	exit(EXIT_FAILURE);
-}
-
-void	ft_handle_args(int ac, char *av)
-{
-	if (ac == 1)
-		ft_error("Invalid number of arguments!\n\
-Try \"--help\" for more information.\n");
-	if (ac == 2)
-	{
-		if (!ft_strncmp(av, "--help", 6))
-		{
-			ft_error("\"push_swap\" requires minimum of one argument \
-passed as fllows:\n$> ./push_swap arg1 arg2 arg3 arg4 ...\n");
-		}
-		else
-			if (!is_str_digit(av) || !is_in_int_range(ft_atoi(av)))
-				ft_error("Error!\n");
-	}
 }
