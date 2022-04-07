af7e23a581b8daeecd09731568ee2641b9be710a
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-04-07 03:08:04 +0000

    Remove ft_push_to_stack function

--- includes/headers/push_swap.h
+++ includes/headers/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: sel-kham <sel-kham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/29 22:36:54 by sel-kham          #+#    #+#             */
-/*   Updated: 2022/04/07 01:14:11 by sel-kham         ###   ########.fr       */
+/*   Updated: 2022/04/07 03:01:12 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,9 @@
 void	ft_error(const char *err_msg);
 int		is_in_int_range(long n);
 char	*is_str_digit(char *s);
-void	ft_parssing_args(int ac, char **av);
+void	ft_parssing_args(int ac, char **av, t_stack **stack);
 t_node	*ft_new_node(int data);
 void	ft_add_node_at_first(t_stack **stack, t_node *new_node);
-void	ft_push_to_stack(t_stack **stack, char **v, int c);
 void	ft_valiate_input(char *s);
 
 #endif
--- src/app/manage_stacks.c
+++ src/app/manage_stacks.c
@@ -6,17 +6,9 @@
 /*   By: sel-kham <sel-kham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/04 22:21:54 by sel-kham          #+#    #+#             */
-/*   Updated: 2022/04/05 23:13:11 by sel-kham         ###   ########.fr       */
+/*   Updated: 2022/04/07 03:01:21 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/headers/push_swap.h"
 
-void	ft_push_to_stack(t_stack **stack, char **v, int c)
-{
-	int		i;
-
-	i = c;
-	while (--i > 0)
-		ft_add_node_at_first(stack, ft_new_node(ft_atoi(v[i])));
-}
