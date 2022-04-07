fd6445d314f56e1620164ada2cba782b9d9ccc94
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-04-07 03:09:57 +0000

    ft_parssing_args works good

--- src/app/manage_inputs.c
+++ src/app/manage_inputs.c
@@ -6,7 +6,7 @@
 /*   By: sel-kham <sel-kham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/04 23:24:30 by sel-kham          #+#    #+#             */
-/*   Updated: 2022/04/07 01:13:09 by sel-kham         ###   ########.fr       */
+/*   Updated: 2022/04/07 03:09:35 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,11 +44,22 @@ char	*is_str_digit(char *s)
 	return (s);
 }
 
-void	ft_parssing_args(int ac, char **av)
+int	ft_is_num_dup(t_node **head, int num)
 {
-	int	i;
+	t_node	*tmp;
 
-	i = 0;
+	tmp = *head;
+	while (tmp)
+	{
+		if (tmp->data == num)
+			return (0);
+		tmp = tmp->next_node;
+	}
+	return (1);
+}
+
+static void	ft_check_args(int ac, char **av)
+{
 	if (ac == 1)
 		ft_error("Invalid number of arguments!\n\
 Try \"--help\" for more information.\n");
@@ -61,11 +72,24 @@ passed as fllows:\n$> ./push_swap arg1 arg2 arg3 arg4 ...\n");
 			if (!is_str_digit(av[1]) || !is_in_int_range(ft_atoi(av[1])))
 				ft_error("Error!\n");
 	}
-	else
+}
+
+void	ft_parssing_args(int ac, char **av, t_stack **stack)
+{
+	int		i;
+	long	num;
+
+	i = ac;
+	ft_check_args(ac, av);
+	if (ac > 2)
 	{
-		while (av[++i])
-			if (!is_str_digit(av[i]) || !is_in_int_range(ft_atoi(av[i])))
+		while (--i)
+		{
+			num = ft_atoi(av[i]);
+			if (!is_str_digit(av[i]) || !is_in_int_range(num)
+				|| !ft_is_num_dup(&(*stack)->head, num))
 				ft_error("Error!\n");
+			ft_add_node_at_first(stack, ft_new_node(num));
+		}
 	}
-	ft_error("Good!\n");
 }
