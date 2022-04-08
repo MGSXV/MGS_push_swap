0465adcda37dcd0358cb7674b8eceeb3ab6300c8
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-04-08 02:46:12 +0000

    Add sorting instructions' functions prototypes

--- includes/headers/push_swap.h
+++ includes/headers/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: sel-kham <sel-kham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/29 22:36:54 by sel-kham          #+#    #+#             */
-/*   Updated: 2022/04/07 05:36:35 by sel-kham         ###   ########.fr       */
+/*   Updated: 2022/04/07 23:38:42 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,23 @@
 # include<limits.h>
 # include "types.h"
 
+// Manage errors functions
 void	ft_error(const char *err_msg, t_stack **stack);
 void	ft_free_all(t_stack **stack);
+// Parssing arguments functions
 int		is_in_int_range(long n);
 char	*is_str_digit(char *s);
 void	ft_parssing_args(int ac, char **av, t_stack **stack);
+// Manage stacks functions
 t_node	*ft_new_node(int data);
 void	ft_add_node_at_first(t_stack **stack, t_node *new_node);
-void	ft_valiate_input(char *s);
+// Stack sorting instructions
+void	swap_stack(t_stack **stack, char s);
+void	sswap_stack(t_stack **stack_a, t_stack **stack_b);
+void	push_stack(t_stack **stack_1, t_stack **stack_2, char s);
+void	rotate_stack(t_stack **stack, char s);
+void	rrotate_stack(t_stack **stack_1, t_stack **stack_2);
+void	reverse_rotate_stack(t_stack **stack, char s);
+void	rreverse_rotate_stack(t_stack **stack_1, t_stack **stack_2);
 
 #endif
--- src/app/manage_stacks.c
+++ src/app/manage_stacks.c
@@ -6,9 +6,52 @@
 /*   By: sel-kham <sel-kham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/04 22:21:54 by sel-kham          #+#    #+#             */
-/*   Updated: 2022/04/07 03:01:21 by sel-kham         ###   ########.fr       */
+/*   Updated: 2022/04/07 23:38:17 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/headers/push_swap.h"
 
+void	swap_stack(t_stack **stack, char s)
+{
+	if (s)
+		ft_printf("s%c\n", s);
+}
+
+void	sswap_stack(t_stack **stack_a, t_stack **stack_b)
+{
+	swap_stack(stack_a, '\0');
+	swap_stack(stack_b, '\0');
+	ft_printf("ss\n");
+}
+
+void	push_stack(t_stack **stack_1, t_stack **stack_2, char s)
+{
+	ft_printf("p%c\n", s);
+}
+
+void	rotate_stack(t_stack **stack, char s)
+{
+	if (s)
+		ft_printf("r%c\n", s);
+}
+
+void	rrotate_stack(t_stack **stack_1, t_stack **stack_2)
+{
+	rotate_stack(stack_1, '\0');
+	rotate_stack(stack_2, '\0');
+	ft_printf("rr\n");
+}
+
+void	reverse_rotate_stack(t_stack **stack, char s)
+{
+	if (s)
+		ft_printf("r%c\n", s);
+}
+
+void	rreverse_rotate_stack(t_stack **stack_1, t_stack **stack_2)
+{
+	reverse_rotate_stack(stack_1, '\0');
+	reverse_rotate_stack(stack_2, '\0');
+	ft_printf("rrr\n");
+}
