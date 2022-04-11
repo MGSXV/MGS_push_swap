64137e21f99aca82397b9dbaa0c142fddcc410e6
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-04-11 03:02:38 +0000

    Create push_swap function & sorting three elements

--- src/app/push_swap.c
+++ src/app/push_swap.c
@@ -6,13 +6,41 @@
 /*   By: sel-kham <sel-kham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/10 21:14:37 by sel-kham          #+#    #+#             */
-/*   Updated: 2022/04/10 21:15:25 by sel-kham         ###   ########.fr       */
+/*   Updated: 2022/04/11 02:59:59 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/headers/push_swap.h"
 
-void	ft_sort_stack(t_stack **stack)
+void	ft_sort_base_case(t_stack **stack_a)
 {
-	
+	if ((*stack_a)->head->data > (*stack_a)->tail->data)
+	{
+		rotate_stack(stack_a, 'a');
+		ft_sort_base_case(stack_a);
+	}
+	if (((*stack_a)->head->next_node->data > (*stack_a)->head->data)
+		&& ((*stack_a)->head->next_node->data > (*stack_a)->tail->data))
+	{
+		reverse_rotate_stack(stack_a, 'a');
+		ft_sort_base_case(stack_a);
+	}
+	if ((*stack_a)->head->data > (*stack_a)->head->next_node->data)
+	{
+		swap_stack(stack_a, 'a');
+		ft_sort_base_case(stack_a);
+	}
+}
+
+void	ft_sort_stack(t_stack **stack_a, t_stack **stack_b)
+{
+	stack_a = stack_b;
+}
+
+void	push_swap(t_stack **stack_a, t_stack **stack_b)
+{
+	if ((*stack_a)->size < 6)
+		ft_sort_base_case(stack_a);
+	else
+		ft_sort_stack(stack_a, stack_b);
 }
