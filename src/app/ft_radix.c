ec86de9c0e16f7ebcad6845827615a823329a823
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-04-13 00:51:16 +0000

    Radinx soring is working now, needs optimization

--- src/app/ft_radix.c
+++ src/app/ft_radix.c
@@ -6,41 +6,31 @@
 /*   By: sel-kham <sel-kham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/12 01:27:20 by sel-kham          #+#    #+#             */
-/*   Updated: 2022/04/12 05:13:22 by sel-kham         ###   ########.fr       */
+/*   Updated: 2022/04/13 00:49:42 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/headers/push_swap.h"
 
-void    ft_radix(t_stack **stack_a, t_stack **stack_b)
+void	ft_radix(t_stack **stack_a, t_stack **stack_b)
 {
-	int	max_num;
 	int	num;
 	int	i;
 	int	j;
+	int	size;
 
-	max_num = (*stack_a)->max;
 	i = -1;
-	while (++i < 32)
+	size = (*stack_a)->size;
+	while (++i < 7)
 	{
 		j = -1;
-		while (++j < (*stack_a)->size)
+		while (++j < size)
 		{
-			num = (*stack_a)->head->data;
-			if (i != 31)
-			{
-				if ((num >> i) & 1)
-					rotate_stack(stack_a, 'a');
-				else
-					push_stack(stack_a, stack_b, 'b');
-			}
-			else
-			{
-				if (!((num >> i) & 1))
-					rotate_stack(stack_a, 'a');
-				else
-					push_stack(stack_a, stack_b, 'b');
-			}
+			num = (*stack_a)->head->index;
+			if (((num >> i) & 1))
+				rotate_stack(stack_a, 'a');
+			else if (!((num >> i) & 1) && !ft_is_sorted(stack_a))
+				push_stack(stack_a, stack_b, 'b');
 		}
 		while ((*stack_b)->head)
 			push_stack(stack_b, stack_a, 'a');
