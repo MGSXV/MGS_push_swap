7001fd2027bcfc9882a4268b3cfb346d8e4c7c64
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-04-13 00:47:24 +0000

    Indexing Indexing stack_a's elements

--- src/helpers/push_swap_utilities.c
+++ src/helpers/push_swap_utilities.c
@@ -6,7 +6,7 @@
 /*   By: sel-kham <sel-kham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/10 21:39:02 by sel-kham          #+#    #+#             */
-/*   Updated: 2022/04/11 23:14:41 by sel-kham         ###   ########.fr       */
+/*   Updated: 2022/04/13 00:47:01 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,17 @@ int	ft_is_sorted(t_stack **stack)
 {
 	t_node	*tmp;
 
-	tmp = (*stack)->head;
-	while (tmp)
+	if (stack && *stack)
 	{
-		if (tmp->data > tmp->next_node->data)
-			return (0);
-		tmp = tmp->next_node;
+		tmp = (*stack)->head;
+		while (tmp)
+		{
+			if (!tmp->next_node)
+				break ;
+			if (tmp->data > tmp->next_node->data)
+				return (0);
+			tmp = tmp->next_node;
+		}
 	}
 	return (1);
 }
@@ -39,4 +44,41 @@ void	ft_get_max_and_min(t_stack **stack, int num)
 	if (num < (*stack)->min)
 		(*stack)->min = num;
 	return ;
+}
+
+int	get_index(int *tab, int num, int size)
+{
+	int	i;
+
+	i = -1;
+	while (++i < size)
+		if (tab[i] == num)
+			return (i);
+	return (-1);
+}
+
+void	ft_indexing(t_stack **stack)
+{
+	t_node	*tmp;
+	int		*tab;
+	int		i;
+
+	tab = malloc(sizeof(int) * (*stack)->size);
+	tmp = (*stack)->head;
+	i = 0;
+	while (tmp)
+	{
+		tab[i] = tmp->data;
+		tmp = tmp->next_node;
+		i++;
+	}
+	ft_insertion_sort(tab, (*stack)->size);
+	tmp = (*stack)->head;
+	while (tmp)
+	{
+		tmp->index = get_index(tab, tmp->data, (*stack)->size) + 1;
+		tmp = tmp->next_node;
+		i++;
+	}
+	free(tab);
 }
