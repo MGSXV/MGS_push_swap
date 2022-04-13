40942f40a31c2ff3e265971b8b2b0420c3937168
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-04-13 01:08:12 +0000

    Add ft_get_bits_num function

--- src/app/ft_radix.c
+++ src/app/ft_radix.c
@@ -6,22 +6,36 @@
 /*   By: sel-kham <sel-kham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/12 01:27:20 by sel-kham          #+#    #+#             */
-/*   Updated: 2022/04/13 00:49:42 by sel-kham         ###   ########.fr       */
+/*   Updated: 2022/04/13 01:07:43 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/headers/push_swap.h"
 
+int	ft_get_bits_num(t_stack *stack)
+{
+	int	bits;
+	int	max;
+
+	bits = 0;
+	max = stack->size;
+	while ((max >> bits))
+		bits++;
+	return (bits);
+}
+
 void	ft_radix(t_stack **stack_a, t_stack **stack_b)
 {
 	int	num;
+	int	bit_num;
 	int	i;
 	int	j;
 	int	size;
 
 	i = -1;
+	bit_num = ft_get_bits_num(*stack_a);
 	size = (*stack_a)->size;
-	while (++i < 7)
+	while (++i < bit_num)
 	{
 		j = -1;
 		while (++j < size)
