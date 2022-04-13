86127ac2e3a504880c7a5f44f71e218f0c31af2d
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-04-13 20:20:08 +0000

    ft_is_increasing

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: sel-kham <sel-kham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/29 22:45:04 by sel-kham          #+#    #+#             */
-/*   Updated: 2022/04/13 00:42:33 by sel-kham         ###   ########.fr       */
+/*   Updated: 2022/04/13 02:38:23 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,7 +40,7 @@ int	main(int c, char **v)
 	stack_a = (t_stack *) malloc(sizeof(t_stack));
 	if (!stack_a)
 		return (0);
-	if (ft_is_sorted(&stack_a))
+	if (ft_is_increasing(&stack_a))
 		ft_error("", &stack_a);
 	stack_b = (t_stack *) malloc(sizeof(t_stack));
 	if (!stack_b)
