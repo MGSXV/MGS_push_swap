37e41ed5e14be4b2f3defb4aa836724b5f33f84a
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-04-22 02:23:30 +0000

    Organizing main

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: sel-kham <sel-kham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/29 22:45:04 by sel-kham          #+#    #+#             */
-/*   Updated: 2022/04/21 20:33:31 by sel-kham         ###   ########.fr       */
+/*   Updated: 2022/04/22 02:21:53 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,17 +69,19 @@ int	main(int c, char **v)
 	stack_a = (t_stack *) malloc(sizeof(t_stack));
 	if (!stack_a)
 		return (0);
-	stack_b = (t_stack *) malloc(sizeof(t_stack));
-	if (!stack_b)
-		return (0);
 	ft_init_stack(&stack_a);
-	ft_init_stack(&stack_b);
 	ft_parssing_args(c, v, &stack_a);
 	if (ft_is_increasing(&stack_a))
 		ft_error("", &stack_a);
-	ft_indexing(&stack_a);
+	stack_b = (t_stack *) malloc(sizeof(t_stack));
+	if (!stack_b)
+		return (0);
+	ft_init_stack(&stack_b);
 	push_swap(&stack_a, &stack_b);
+	printf("STACK A:\n");
 	ft_print_stack(&stack_a);
+	printf("STACK B:\n");
+	ft_print_stack(&stack_b);
 	ft_free_all(&stack_b);
 	ft_free_all(&stack_a);
 	return (0);
