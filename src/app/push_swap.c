5b61363fa82ded68a70e7690ed72b0da95ac46f8
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-04-23 21:35:36 +0000

    Managing non-base cases

--- src/app/push_swap.c
+++ src/app/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: sel-kham <sel-kham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/10 21:14:37 by sel-kham          #+#    #+#             */
-/*   Updated: 2022/04/21 23:48:04 by sel-kham         ###   ########.fr       */
+/*   Updated: 2022/04/23 02:42:39 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,15 +32,21 @@ void	ft_sort_base_case(t_stack **stack_a)
 	}
 }
 
-void	ft_sort_stack(t_stack **stack_a, t_stack **stack_b)
-{
-	ft_push_to_stack_b(stack_a, stack_b);
-}
+// void	ft_sort_stack(t_stack **stack_a, t_stack **stack_b)
+// {
+// 	ft_push_to_stack_b(stack_a, stack_b);
+// }
 
 void	push_swap(t_stack **stack_a, t_stack **stack_b)
 {
 	if ((*stack_a)->size < 4)
 		ft_sort_base_case(stack_a);
 	else
-		ft_sort_stack(stack_a, stack_b);
+	{
+		ft_sorted_indexing(stack_a);
+		ft_push_to_stack_b(stack_a, stack_b);
+		while ((*stack_b)->size)
+			ft_best_move(stack_a, stack_b);
+		put_lowest_to_top(stack_a);
+	}
 }
